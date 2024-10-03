0e013d4e222c32f414ec6583e3f7cb34a12bca6f
t <ajeymalviya143@gmail.com>
2024-10-03 21:21:21 +0530

    added new changes in main file with respect to server socket

--- app/main.py
+++ app/main.py
@@ -8,14 +8,16 @@ def main():
     # Uncomment this to pass the first stage
     #
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    # server_socket.accept() # wait for client
-    client, addr = server_socket.accept()
-    # print("client and addr are :", client,addr)
+    print(server_socket)
+    server_socket.accept() # wait for client
+    # client, addr = server_socket.accept()
+    print(server_socket.accept)
+    
     client.send(b"+PONG\r\n")
-    for conn, address in next_client(socket_server):
-        for message in next_message(conn, address):
-            handle_message(message, conn, address)  # type: ignore
-            raise CloseServer("Done")
+    # for conn, address in next_client(socket_server):
+    #     for message in next_message(conn, address):
+    #         handle_message(message, conn, address)  # type: ignore
+    #         raise CloseServer("Done")
 
 if __name__ == "__main__":
     main()
