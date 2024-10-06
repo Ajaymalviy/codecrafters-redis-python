71553b6e286d0634d6f39ef390a0ee006b1cddfd
t <ajeymalviya143@gmail.com>
2024-10-07 01:33:25 +0530

    added new code with try except

--- app/main.py
+++ app/main.py
@@ -2,26 +2,19 @@ import socket
 
 def main():
     print("Logs from your program will appear here!")
-
-    # Create server socket
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.bind(("localhost", 6379))
     server_socket.listen()
-
     print("Server started, waiting for connections...")
-
     while True:
         client, addr = server_socket.accept()  # Accept a connection
         print(f"Accepted connection from {addr}")
-
         try:
             while True:
                 data = client.recv(1024)  # Receive data from the client
                 if not data:
                     print(f"Connection closed by {addr}")
                     break  # No data means the client has closed the connection
-
-                # Check if the received command is PING in RESP format
                 if data.strip() == b'*1\r\n$4\r\nPING\r\n':
                     client.send(b"+PONG\r\n")  # Send the response
                 else:
