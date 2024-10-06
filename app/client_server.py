c4469586139f68a28e1216fcc52caeccb64f738e
t <ajeymalviya143@gmail.com>
2024-10-07 00:49:59 +0530

    added new file client server which is create for testing the server

---
+++ app/client_server.py
@@ -0,0 +1,16 @@
+import socket
+
+def test_client():
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect(("localhost", 6379))
+    
+    for _ in range(5):  # Send multiple PING commands
+        client_socket.send(b'PING\r\n')
+        response = client_socket.recv(1024)
+        print(response.decode())
+
+    client_socket.close()
+
+if __name__ == "__main__":
+    test_client()
+
