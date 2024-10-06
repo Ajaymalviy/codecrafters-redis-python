412a9a9597cc2f14cb477bd1423bd25e95644e19
t <ajeymalviya143@gmail.com>
2024-10-06 17:04:49 +0530

    added new changes into main file for connection

--- app/main.py
+++ app/main.py
@@ -1,23 +1,20 @@
 import socket  # noqa: F401
 
-
 def main():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
 
-    # Uncomment this to pass the first stage
-    #
+    # Create server socket
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    print(server_socket)
-    server_socket.accept() # wait for client
-    # client, addr = server_socket.accept()
-    print(server_socket.accept)
-    
-    client.send(b"+PONG\r\n")
-    # for conn, address in next_client(socket_server):
-    #     for message in next_message(conn, address):
-    #         handle_message(message, conn, address)  # type: ignore
-    #         raise CloseServer("Done")
+    print("Server started, waiting for connections...")
+
+    while True:
+        client, addr = server_socket.accept()  # Accept a connection
+        print(f"Accepted connection from {addr}")
+
+        # Send response to the client
+        client.send(b"+PONG\r\n")
+        client.close()  # Close the client connection
 
 if __name__ == "__main__":
     main()
+
